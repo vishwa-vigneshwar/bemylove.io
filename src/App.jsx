6e0032c53d8eacbb9489e6917468f2973ac1f3ed
Vishwa <84085745+vishwa-vigneshwar@users.noreply.github.com>
2024-06-29 14:30:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
+      href=""
       target="__blank"
     >
       Made with{" "}
